26fea6dc2c7e69038fcbfb34faab5c495ef33f6a
shreyasrama <shreyas.rama1@gmail.com>
2025-08-23 15:53:39 +1200

    add delays endpoint and db function

--- api/db.py
+++ api/db.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 async def get_latest(db_path: str):
-    print("calling db")
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
 
@@ -21,5 +20,27 @@ async def get_latest(db_path: str):
     """)
 
     latest_calls = cur.fetchall()
-
     return latest_calls
+
+async def get_delays(db_path: str, start_date: str, end_date: str, delay: int):
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+
+    cur.execute("""
+    SELECT 
+        direction_ref, 
+        published_line_name, 
+        stop_point_ref, 
+        stop_point_name, 
+        aimed_departure_time, 
+        actual_departure_time 
+    FROM bus_calls 
+    WHERE response_timestamp BETWEEN ? AND ?
+    AND (
+        julianday(substr(replace(actual_departure_time, 'T', ' '), 1, 19)) - 
+        julianday(substr(replace(aimed_departure_time, 'T', ' '), 1, 19))
+    ) * 24 * 60 > ?;
+    """, (start_date, end_date, delay))
+
+    calls = cur.fetchall()
+    return calls
--- api/main.py
+++ api/main.py
@@ -2,11 +2,11 @@ import os
 from aiocache import cached
 from fastapi import FastAPI, HTTPException, Security, status
 from fastapi.security import APIKeyHeader
-from functools import lru_cache
 
-from db import get_latest
+from db import get_latest, get_delays
 from info import get_diagnostic_info
 
+# Set up API Key to be passed in via a header
 API_KEY = os.getenv("API_KEY")
 api_key_header = APIKeyHeader(name="X-API-Key", auto_error=True)
 
@@ -17,15 +17,29 @@ async def get_api_key(api_key: str = Security(api_key_header)):
         )
     return api_key
 
+# Create app object
 app = FastAPI()
 
-@app.get("/info")
+# region Routes
+
+@app.get("/info", description="Returns log output and database size.")
 async def info():
     log_path = os.getenv("LOG_PATH") or ""
-    return await get_diagnostic_info(log_path=log_path)
+    db_path = os.getenv("DB_PATH") or ""
 
-@app.get("/latest")
-@cached(ttl=10)
+    return await get_diagnostic_info(log_path=log_path, db_path=db_path)
+
+@app.get("/latest", description="Returns all entries that have the most recent response_timestamp.")
+@cached(ttl=60)
 async def latest(api_key: str = Security(get_api_key)):
     db_path = os.getenv("DB_PATH") or ""
+
     return {'latest': await get_latest(db_path=db_path)}
+
+@app.get("/delays/", description="Returns all bus calls within a specified date range and delay amount.")
+async def delays(start: str, end: str, delay: int = 3, api_key: str = Security(get_api_key)):
+    db_path = os.getenv("DB_PATH") or ""
+
+    return {'delays': await get_delays(db_path=db_path, start_date=start, end_date=end, delay=delay)}
+
+# endregion
