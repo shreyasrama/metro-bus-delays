93ee150a52dc4404756e539aaafba21cd9520e34
shreyasrama <shreyas.rama1@gmail.com>
2025-08-23 15:53:25 +1200

    return database size in info

--- api/info.py
+++ api/info.py
@@ -1,11 +1,22 @@
-import json
+import os
 
-async def get_diagnostic_info(log_path: str):
+async def get_diagnostic_info(log_path: str, db_path: str):
     output = {}
 
     # Get log data
     with open(log_path, 'r') as log_file:
+        logs = []
+        
+        # Remove newline and split each line into [timestamp, level, message]
         lines = log_file.readlines()
-        output['logs'] = json.loads(json.dumps(lines[-150:]))
+        for line in lines[-150:]:
+            line = line.strip()  # Remove newline
+            parts = line.split(' - ', 2) # Split by ' - ', max 2 splits
+            logs.append(parts)
+
+        output['logs'] = logs
+
+    # Get database file size
+    output['db_size'] = os.path.getsize(db_path) / 1024 / 1024
 
     return output
